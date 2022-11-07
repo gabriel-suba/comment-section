e82d7c57e0d972a2b4803777d5fc39d0722508ae
gab <wr3gab@gmail.com>
2022-11-07 16:21:41 +0800

    logout function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import { useState, useRef, useContext } from 'react'
 import { DataContext } from './contexts/dataContext'
+import { logOut } from './fb'
 import Comments from './components/comments/Comments'
 import Login from './components/login/Login'
 import Modal from './components/modal/Modal'
-import Nav from './components/nav/Nav'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGear, faRightFromBracket, faUser } from '@fortawesome/free-solid-svg-icons'
 import './App.css'
 
+library.add(faGear, faRightFromBracket, faUser)
+
 const App = () => {
-	const { loggedIn, loading } = useContext(DataContext)
+	const { loggedIn, loading, setLoggedIn } = useContext(DataContext)
 	const [selected, setSelected] = useState({ collection: null, docId: null, replies: [] })
+	const [openOption, setOpenOption] = useState(false)
 	const modalRef = useRef(null)
 
 	function handleOpenModal(collection, docId, replies = []) {
 		modalRef.current.classList.toggle('open-modal')
 		setSelected({ collection: collection, docId: docId, replies: replies })
+	}
+
+	function handleLogout() {
+		logOut()
+		setLoggedIn(false)
+		setOpenOption(false)
 	}
 
 	if (loading) return <div className="App"><div>loading...</div></div>
@@ -22,6 +34,7 @@ const App = () => {
 	
 	return (
 		<div className="App">
+			<div onClick={handleLogout} className="logout-btn container"><FontAwesomeIcon icon="fa-solid fa-right-from-bracket" /> Logout</div>
 			<Modal ref={modalRef} selected={selected} setSelected={setSelected} />
 			<main className="container">
 				<Comments handleOpenModal={handleOpenModal} />
